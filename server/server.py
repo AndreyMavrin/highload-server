b56fd9a1ef2d2fc8f1e27eebdef4a05d5b0c2c8a
Andrey_Mavrin <andmavrin@yandex.ru>
2021-03-25 07:33:50 +0300

    on port 8080

--- server/server.py
+++ server/server.py
@@ -20,6 +20,7 @@ class Server:
 
     def prepare_socket(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.host, self.port))
         self.socket.listen(self.max_connections)
         print('Starting listen socket')
